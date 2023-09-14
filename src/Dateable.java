6ebe25a70d22f904520745703d97724d9fc86ad1
AP <Alexanderp2026@student.cis.edu.hk>
2023-09-14 10:25:22 +0800

    hi

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        System.out.println("Input your age:");
+        age = scanner.nextInt();
+        System.out.println(7+(age/2));
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int pages;
+        System.out.println("Please input your age:");
+        pages = scanner.nextInt();
+        System.out.println(100-pages);
     }
 }
